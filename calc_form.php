6f37c37adb7dbafb0b26e946666b8db067d963b9
akiho-o <akiho.o.0930@gmail.com>
2021-03-16 00:41:35 +0900

    再提出

--- calc_form.php
+++ calc_form.php
@@ -9,15 +9,15 @@
     <h1>計算アプリ</h1>
     <h2>計算式を入力してください</h2>
     <form name="form1" action="answer.php" method="post">
-        <input type = "number" required="required" name = "num1">
+        <input type="number" required="required" name = "num1">
         <select name="selOpe" size=1>
-            <option value = "＋">＋</option>
-            <option value = "－">－</option>
-            <option value = "×">×</option>
-            <option value = "÷">÷</option>
+            <option value="＋">＋</option>
+            <option value="－">－</option>
+            <option value="×">×</option>
+            <option value="÷">÷</option>
         </select>
-        <input type = "number" required="required" name = "num2">
-        <input type = "submit" value = "計算">
+        <input type="number" required="required" name="num2">
+        <input type="submit" value="計算">
     </form>
 </body>
 </html>
